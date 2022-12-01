4a3441016db64e1fc5e77f057091fb26d762721f
imam.hermawan <imam.hermawan@gramedia.id>
2022-12-01 22:13:57 +0700

    add Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Header from './Components/UI/Header';
 import { BrowserRouter } from 'react-router-dom';
+
+import Header from './Components/UI/Header';
+import Footer from './Components/UI/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -1,17 +1,19 @@
 const Footer = () => {
-  <footer className="footer">
-    <div className="footer__logo">
-      <h1>TOUR-KUY</h1>
-    </div>
-    <ul className="footer__nav">
-      <li><a href="/"></a>About Us</li>
-      <li><a href="/"></a>Download apps</li>
-      <li><a href="/"></a>Become a guide</li>
-      <li><a href="/"></a>Careers</li>
-      <li><a href="/"></a>Contact</li>
-    </ul>
-    <p className="footer__copyright">&copy; For Technical Test to join Insignia.co.id</p>
-  </footer>    
+  return (
+    <footer className="footer">
+      <div className="footer__logo">
+        <h1>TOUR-KUY</h1>
+      </div>
+      <ul className="footer__nav">
+        <li><a href="/">About Us</a></li>
+        <li><a href="/">Download apps</a></li>
+        <li><a href="/">Become a guide</a></li>
+        <li><a href="/">Careers</a></li>
+        <li><a href="/">Contact</a></li>
+      </ul>
+      <p className="footer__copyright">&copy; Imam Taufiq Hermawan Purpose For Technical Test to join <a href="https://insignia.co.id/">INSIGNIA</a></p>
+    </footer>
+  )
 };
 
 export default Footer;
