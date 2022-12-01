40dd297f280c240a8e10b605ca2688d0fe9db652
imam.hermawan <imam.hermawan@gramedia.id>
2022-12-02 02:45:17 +0700

    add register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Tour from './Components/Tour/Tour';
 import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Tour />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,25 +1,33 @@
 import Header from "../UI/Header";
 import Footer from "../UI/Footer";
 
-const Login = () => {
+const Register = () => {
   return (
     <>
       <Header />
 
       <div className="main">
         <div className="login-form">
-          <h2 className="heading-secondary ma-bt-lg">Log into your account</h2>
+          <h2 className="heading-secondary ma-bt-lg">Register</h2>
           <form className="form form--login">
+            <div className="form__group">
+              <label className="form__label">Name</label>
+              <input id="name" className="form__input" type="text" placeholder="imam" />
+            </div>
             <div className="form__group">
               <label className="form__label">Email Address</label>
               <input id="email" className="form__input" type="email" placeholder="imam@mail.com" />
             </div>
+            <div className="form__group">
+              <label className="form__label">Phone Number</label>
+              <input id="phone" className="form__input" type="text" placeholder="081212212121" />
+            </div>
             <div className="form__group ma-bt-md">
               <label className="form__label">Password</label>
-              <input id="password" className="form__input" type="email" placeholder="rahasia123" />
+              <input id="password" className="form__input" type="password" placeholder="rahasia123" />
             </div>
             <div className="form__group">
-              <button className="btn btn--green">Login</button>
+              <button className="btn btn--green">Register</button>
             </div>
           </form>
         </div>
@@ -30,4 +38,4 @@ const Login = () => {
   )
 };
 
-export default Login;
+export default Register;
