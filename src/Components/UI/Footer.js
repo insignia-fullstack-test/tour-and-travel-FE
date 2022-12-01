b4db6381b23f199fda85eb1b874be11c6ca90035
imam.hermawan <imam.hermawan@gramedia.id>
2022-12-01 22:07:07 +0700

    import font, css and add header component

---
+++ src/Components/UI/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  <footer className="footer">
+    <div className="footer__logo">
+      <h1>TOUR-KUY</h1>
+    </div>
+    <ul className="footer__nav">
+      <li><a href="/"></a>About Us</li>
+      <li><a href="/"></a>Download apps</li>
+      <li><a href="/"></a>Become a guide</li>
+      <li><a href="/"></a>Careers</li>
+      <li><a href="/"></a>Contact</li>
+    </ul>
+    <p className="footer__copyright">&copy; For Technical Test to join Insignia.co.id</p>
+  </footer>    
+};
+
+export default Footer;
---
+++ src/Components/UI/Header.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+    <header className="header">
+      <nav className="nav--tours">
+        {/* <p className="nav__el"><Link to='/test'>All Tours</Link></p> */}
+        <a className="nav__el" href="/">All Tours</a>
+      </nav>
+      <nav className="nav nav--user">
+        {
+          isLoggedIn
+            ? <>
+              <a className="nav__el nav__el--logout" href='/'>Log out</a>
+              <a className="nav__el" href='/me'>
+                <span>Hi, NAMA USER</span>
+              </a>
+            </>
+            : <>
+              <a className="nav__el" href="/login">Log in</a>
+              <a className="nav__el nav__el--cta" href="/register">Register</a>
+            </>
+        }
+      </nav>
+    </header >
+  )
+};
+
+export default Header;
